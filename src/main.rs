753b8b83191120bb6fb86f87c57a53cf86de8b2f
millsky <kyle83567@gmail.com>
2020-04-25 18:12:27 -0400

    Seperate Cassandra Auth

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,7 @@ use warp::{sse::ServerSentEvent, Filter};
 use std::time::Duration;
 use tokio::time::interval;
 
-extern crate cdrs;
-
-// Cassandra Database Setup
-use cdrs::authenticators::StaticPasswordAuthenticator;
-use cdrs::cluster::session::{new as new_session};
-use cdrs::cluster::{ClusterTcpConfig, NodeTcpConfigBuilder};
-use cdrs::load_balancing::RoundRobin;
-use cdrs::query::*;
-use retry::{retry, delay::Fixed, OperationResult};
+mod cassandra;
 
 // create server-sent event
 fn sse_counter(counter: u64) -> Result<impl ServerSentEvent, Infallible> {
@@ -36,33 +28,7 @@ async fn main() {
     let port : u16 = get_port();
 
     println!("Server is starting up on {}", port);
-
-    // Authenticate to our Cassandra Cluster, as configured in the docker-compose file
-    // TODO: Move this into it's own crate
-    let authenticator = StaticPasswordAuthenticator::new("cassandra", "cassandra");
-    // Connect to the Auth Seeder Node
-    let node = NodeTcpConfigBuilder::new("cassandra-node1:9042", authenticator).build();
-    let cluster_config = ClusterTcpConfig(vec![node]);
-    // Attempt to connect 10 times, it's important to handle this from the service level
-    // For debugging purposes Health-Checks can be seen by running `docker ps` on the compose stack
-    let my_session = retry(Fixed::from_millis(30000).take(10), || {
-        println!("Looping... Attempting to authenticate to Apache Cassandra Instance");
-        return match new_session(&cluster_config, RoundRobin::new()) {
-            Ok(val) => OperationResult::Ok(val),
-            Err(e) => {
-                println!("{}", e);
-                return OperationResult::Retry("Not Up Yet");
-            },
-        };
-    }).unwrap();
-
-    println!("Cassandra Instance is up and running, attempting to query");
-
-    let create_ks: &'static str = "CREATE KEYSPACE IF NOT EXISTS test_ks WITH REPLICATION = { \
-                                   'class' : 'SimpleStrategy', 'replication_factor' : 1 };";
-
-    my_session.query(create_ks);
-
+    let my_session : cassandra::CassandraSession = cassandra::authenticate();
     let health = warp::path!("health")
         .map(|| format!("All Good!"));
 
